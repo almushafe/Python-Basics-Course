7823a2c953d109cbf0751bd957fdc559aca8e4e4
user.name "Muhammad Bahi <et blame>
2025-07-11 21:53:48 +0500

    ok

--- function.py
+++ function.py
@@ -1,2 +1,12 @@
 # Python ma Function kya hota ha
 #  python ma fucntion eik code block hota hai jo kisi khas kam karna ka lya banya jata hai
+
+# aur koi kam ham na bar bar karna ho to ham function ka istemal karte hain
+
+# function ka istemal karne se hamara code chota aur asan ho jata hai
+
+# function ki basics 
+def function_name():
+    # yahan par ham function ka code likhte hain
+    print("Hello, World!")
+
