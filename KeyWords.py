b8772696f23d12839cb067622d9f92356a227ab0
user.name "Muhammad Bahi <et blame>
2025-07-11 22:33:09 +0500

    ok

---
+++ KeyWords.py
@@ -0,0 +1,14 @@
+"""
+False      await      else       import     pass
+None       break      except     in         raise
+True       class      finally    is         return
+and        continue   for        lambda     try
+as         def        from       nonlocal   while
+assert     del        global     not        with
+async      elif       if         or         yield
+
+"""
+import keyword
+
+print(len(keyword.kwlist))  # Prints the number of keywords in Python
+print(keyword.kwlist)       # Prints the list of keywords in Python
--- function.py
+++ function.py
@@ -1,3 +1,12 @@
+# Python Function Tutorial
+"""
+https://www.geeksforgeeks.org/python/advanced-python-tutorials/
+
+https://ipython.ai/advanced-guide-python-functions-scope-beyond/
+
+https://www.geeksforgeeks.org/python/python-variables/
+"""
+
 # Python ma Function kya hota ha
 #  python ma fucntion eik code block hota hai jo kisi khas kam karna ka lya banya jata hai
 
@@ -10,3 +19,23 @@ def function_name():
     # yahan par ham function ka code likhte hain
     print("Hello, World!")
 
+#  def eik keyword ha aur ya function define karna ka lya istemal hota hai
+#  aur def keyword likhna ka baad function ka naam likhte hain
+#  function ka naam likhne ka baad parentheses () lagate hain 
+# aur parenthse ka ander jo likhta han eis ko khta han input lana  ya argumant value (optional)
+#  colon : (:) lagana na bhoolna 
+# aur colon ka matlab hota hai ke ab function ka body shuru ho raha hai
+# return output ka lya ya wapis karna ka lya istaml hota hai
+
+
+def greet(name):
+    """
+    function ka name greet hai aur yeh eik argument name leta hai
+    """
+    print(f"Hello, {name}!")  # Function body: prints a greeting message printing the name
+
+# Function call karna ka traiqa bohat simple ha ap us function ka naam likho aur parentheses () lagao
+
+# eik argumant dain kyon ka apna function bana ta howa argument dya tha name ka nam sa eis lya ab ap yahan eis argument ma eik string  pass YA KUCH BHI  karin agin
+
+greet('Alice')  # Function call: passing 'Alice' as an argument to the function
